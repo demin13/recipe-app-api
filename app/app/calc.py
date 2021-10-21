24ac24dee3256de7412a4a9ba0228e03b2010c2e
shekhar Anand <shekhar4321anand@gmail.com>
2021-10-21 22:21:25 +0530

    added tests.py and calc.py

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,2 @@
-from django.test import TestCase
-
-from .calc import add
-
-
-class CalcTest(TestCase):
-    
-    def test_add_numbers(self):
-        self.assertEqual(add(3, 8), 10)
+def add(x, y):
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+from .calc import add
+
+
+class CalcTest(TestCase):
+    
+    def test_add_numbers(self):
+        self.assertEqual(add(3, 8), 10)
