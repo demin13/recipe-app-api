469ae1f2943787484d4fbbe909493f92b83662ea
shekhar Anand <shekhar4321anand@gmail.com>
2021-10-21 22:09:44 +0530

    written function in class

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
 from .calc import add
-
-def test_add_numbers(self):
-
-    self.assertEqual(add(3,8),10)
+class CalcTest:
+    def test_add_numbers(self):
+        self.assertEqual(add(3,8),10)
